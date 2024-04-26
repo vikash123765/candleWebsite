5a2be4c43f07cf11c00aea233d65a01693325f9e
vikash123765 <92863236+vikash123765@users.noreply.github.com>
2024-04-26 08:46:30 -0800

    changed corsconfig to see if tjhat solved the cors issue

--- mobileCaseBackendAlternative/src/main/java/com/vikash/mobileCaseBackend/config/CorsConfig.java
+++ mobileCaseBackendAlternative/src/main/java/com/vikash/mobileCaseBackend/config/CorsConfig.java
@@ -4,12 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {@Override
-public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-            .allowedOrigins("https://api.vtscases.com")
-            .allowedMethods("GET", "POST", "PUT", "DELETE")
-            .allowedHeaders("*"); // You can specify specific headers if needed
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://api.vtscases.com", "https://vtscases.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
 }
-}
-
